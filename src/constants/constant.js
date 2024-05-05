644b154e1dda5bd30cbe7734cffe0cd61a94bdac
Anshu Kumar <akwva01@gmail.com>
2024-05-06 02:39:01 +0530

    filter based on min salary

---
+++ src/constants/constant.js
@@ -0,0 +1,5 @@
+const minSalary = [0, 10, 20, 30, 40, 50, 60, 70];
+const experienceData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const employeeData = [[1, 10], [11, 20], [21, 50], [51, 100], [101, 200], [201, 500], [501]];
+
+export { minSalary, experienceData, employeeData };
--- src/pages/jobs/Jobs.jsx
+++ src/pages/jobs/Jobs.jsx
@@ -1,15 +1,40 @@
 import { useState, useEffect } from "react";
 import JobCard from "../../components/jobcard/JobCard";
 import styles from "./jobs.module.css";
+import { minSalary } from "../../constants/constant";
+
+import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const Jobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [filteredjobs, setFilteredjobs] = useState([]);
+    const [minBasePay, setMinBasePay] = useState("none");
+
+    const handleMinBasePay = (e) => {
+        setMinBasePay(e.target.value);
+    };
+
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     const body = JSON.stringify({
         limit: 10,
         offset: 0,
     });
+
+    const handleFilter = () => {
+        let temp = [];
+        if (minBasePay !== "none") {
+            temp = jobs.filter((job) => job.minJdSalary >= minBasePay);
+            setFilteredjobs(temp);
+        }
+    };
+
+    useEffect(() => {
+        handleFilter();
+    }, [minBasePay]);
 
     const requestOptions = {
         method: "POST",
@@ -25,7 +50,31 @@ const Jobs = () => {
     }, []);
 
     return (
-        <div className={styles.jobs}>{jobs && jobs?.map((job) => <JobCard key={job.jdUid} jobDetails={job} />)}</div>
+        <div className="">
+            <div className={styles.filter}>
+                <Box sx={{ minWidth: 240 }}>
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        {minBasePay !== "none" && <p className="">Min Base Pay</p>}
+                        <Select value={minBasePay} onChange={handleMinBasePay}>
+                            <MenuItem value="none" style={{ display: "none" }}>
+                                Minimum Base Pay
+                            </MenuItem>
+                            {minSalary.map((item, idx) => (
+                                <MenuItem key={idx} value={item}>
+                                    {`${item}L`}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
+            </div>
+
+            <div className={styles.jobs}>
+                {filteredjobs.length > 0
+                    ? filteredjobs?.map((job) => <JobCard key={job.jdUid} jobDetails={job} />)
+                    : jobs?.map((job) => <JobCard key={job.jdUid} jobDetails={job} />)}
+            </div>
+        </div>
     );
 };
 export default Jobs;
